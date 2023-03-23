6dc3dd9fbaaa55f938e93ebaf09cc326c7b2d957
hasan <hasan.softopark@gmail.com>
2023-03-23 13:16:28 +0600

    multyple video ulpoad done

--- src/Components/CoursePost/CoursVideo/UploadCoursVideo.js
+++ src/Components/CoursePost/CoursVideo/UploadCoursVideo.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-const UploadCoursVideo = () => {
-  return <div></div>;
-};
+function UploadCoursVideo() {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+  const [progress, setProgress] = useState(0);
+  const [courseName, setCourseName] = useState("");
+
+  const handleFileUpload = (event) => {
+    setSelectedFiles(event.target.files);
+  };
+
+  const handleCourseName = (event) => {
+    setCourseName(event.target.value);
+  };
+
+  const uploadFiles = async () => {
+    const formData = new FormData();
+    for (let i = 0; i < selectedFiles.length; i++) {
+      formData.append("videos", selectedFiles[i]);
+    }
+    formData.append("courseName", courseName);
+    try {
+      const response = await axios.put(
+        "http://localhost:5000/coursvideo",
+        formData,
+        {
+          onUploadProgress: (progressEvent) => {
+            const percentCompleted = Math.round(
+              (progressEvent.loaded * 100) / progressEvent.total
+            );
+            setProgress(percentCompleted);
+          },
+        }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Course Name"
+        onChange={handleCourseName}
+      />
+      <br />
+      <input type="file" multiple onChange={handleFileUpload} />
+      <button onClick={uploadFiles}>Upload</button>
+      {progress > 0 && <p>Uploading: {progress}%</p>}
+    </div>
+  );
+}
 
 export default UploadCoursVideo;
--- src/Routers/Route/Route.js
+++ src/Routers/Route/Route.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import AcademyPost from "../../Components/AcademyPost/AcademyPost";
 import CoursePost from "../../Components/CoursePost/CoursePost";
+import UploadCoursVideo from "../../Components/CoursePost/CoursVideo/UploadCoursVideo";
 import Getacademycours from "../../Components/CoursePost/Show-cours/Getacademycours";
 import Dashboard from "../../Components/Dashboard/Dashboard";
 import Users from "../../Components/Users/Users";
@@ -32,9 +33,13 @@ const routers = createBrowserRouter([
       },
 
       {
-        path: "/coursePost/academiccours",
+        path: "/coursePost/academicours",
         element: <Getacademycours></Getacademycours>,
       },
+      {
+        path:"/coursePost/academicours/video",
+        element: <UploadCoursVideo></UploadCoursVideo>
+      }
     ],
   },
 ]);
