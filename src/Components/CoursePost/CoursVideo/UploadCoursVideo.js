4155be6be4168c232606b68fd6e77b0078ef6975
hasan <hasan.softopark@gmail.com>
2023-03-25 15:46:00 +0600

    vidoe upload methood done

--- src/Components/CoursePost/CoursVideo/UploadCoursVideo.js
+++ src/Components/CoursePost/CoursVideo/UploadCoursVideo.js
@@ -1,77 +1,90 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-function UploadCoursVideo({ data }) {
-  console.log(data._id + "this is upload video");
+const UploadCoursVideo = ({ data }) => {
+  const [chapterName, setChapterName] = useState("");
+  const [videos, setVideos] = useState([]);
+  const [uploadProgress, setUploadProgress] = useState(0);
 
-  const [selectedFiles, setSelectedFiles] = useState([]);
-  const [progress, setProgress] = useState(0);
-  const [courseName, setCourseName] = useState("");
-
-  const handleFileUpload = (event) => {
-    setSelectedFiles(event.target.files);
+  const handlechapterNameChange = (e) => {
+    setChapterName(e.target.value);
   };
 
-  const handleCourseName = (event) => {
-    setCourseName(event.target.value);
+  const handleVideoChange = (e) => {
+    setVideos(e.target.files);
   };
 
-  const uploadFiles = async () => {
+  const handleUploadProgress = (event) => {
+    const percentCompleted = Math.round((event.loaded * 100) / event.total);
+    setUploadProgress(percentCompleted);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
     const formData = new FormData();
-    for (let i = 0; i < selectedFiles.length; i++) {
-      formData.append("videos", selectedFiles[i]);
+
+    for (let i = 0; i < videos.length; i++) {
+      formData.append("videos", videos[i]);
     }
-    formData.append("courseName", courseName);
-    try {
-      const response = await axios.put(
-        `http://localhost:5000/coursvideo`,
-        formData,
-        {
-          onUploadProgress: (progressEvent) => {
-            const percentCompleted = Math.round(
-              (progressEvent.loaded * 100) / progressEvent.total
-            );
-            setProgress(percentCompleted);
-          },
-        }
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
+
+    formData.append("chapterName", chapterName);
+    formData.append("courseId", data?._id);
+
+    axios
+      .post("http://localhost:5000/coursvideo", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        onUploadProgress: handleUploadProgress,
+      })
+      .then((response) => {
+        console.log(response.data);
+        // handle success
+      })
+      .catch((error) => {
+        console.log(error);
+        // handle error
+      });
   };
 
   return (
-    <>
-      <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-      <div className="modal">
-        <div className="modal-box relative">
-          <label
-            htmlFor="my-modal-3"
-            className="btn btn-sm btn-circle absolute right-2 top-2"
-          >
-            ✕
-          </label>
-          <div className="py-10">
-            <div className=" block">
-              <input
-                type="text"
-                className="input input-bordered input-info w-full mt-8 mb-3 "
-                placeholder="Chapter  Name"
-                onChange={handleCourseName}
-              />
-              <br />
-              <input type="file" multiple onChange={handleFileUpload} />
-            </div>
-            <button className="btn  m-auto block my-4" onClick={uploadFiles}>
-              Upload
-            </button>
-            {progress > 0 && <p>Uploading: {progress}%</p>}
-          </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="courseName">Course Name:</label>
+        <input
+          type="text"
+          id="chapterName"
+          name="chapterName"
+          value={chapterName}
+          onChange={handlechapterNameChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="videos">Videos:</label>
+        <input
+          type="file"
+          id="videos"
+          name="videos"
+          multiple
+          onChange={handleVideoChange}
+        />
+      </div>
+      <div className="progress">
+        <div
+          className="progress-bar"
+          role="progressbar"
+          style={{ width: `${uploadProgress}%`, backgroundColor: "green" }}
+          aria-valuenow={uploadProgress}
+          aria-valuemin="0"
+          aria-valuemax="100"
+        >
+          {uploadProgress}%
         </div>
       </div>
-    </>
+      <button type="submit">Upload Videos</button>
+    </form>
   );
-}
+};
 
 export default UploadCoursVideo;
